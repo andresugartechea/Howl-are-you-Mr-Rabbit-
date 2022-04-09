cc10be686451db313f11ee40a1bf030031195f92
fnassar <75119980+fnassar@users.noreply.github.com>
2022-04-09 22:25:04 +0400

    .

--- public/zbackground.js
+++ public/zbackground.js
@@ -5,17 +5,21 @@ let height = window.innerHeight;
 
 // bg image
 let bg;
+let myCanvas;
 
 // create leaves array to randomly assign leaf objects 
 let leaves = [];
 
 function setup() {
     //Set the parent of the canvas to an exisitng html element's id value 
-    const myCanvas = createCanvas(width, height);
+    myCanvas = createCanvas(width, height);
     myCanvas.parent("sketch");
     bg = loadImage('images/background.png');
+    background(bg);
 }
 
 function draw() {
+    myCanvas = resizeCanvas(window.innerWidth, window.innerHeight)
     background(bg);
+
 }
--- public/zfallingObj.js
+++ public/zfallingObj.js
@@ -1,26 +1,52 @@
 class FallingObj {
-    constructor(name, roomName, x, r, n) {
+    constructor(name, roomName, x, n) {
         this.loc = {
             x: x,
             y: 0
         }
         this.name = name;
-        this.r = r; // image size
-        this.g = window.innerHeight - window.innerHeight * 0.11; // end of screen
-        this.speed = 0; // velocity this.step(prev name)
-        this.n = 0; // stores array count of images so it can be easily accessible with sockets
-        this.image = "/images/leaf" + (n) + ".png";
+        this.g = window.innerHeight - window.innerHeight * 0.1; // end of screen +image height
+        this.speed = {
+            x: random(-3, 3),
+            y: 0
+        }; // velocity this.step(prev name)
+        this.n = n; // stores array count of images so it can be easily accessible with sockets
+        this.img = loadImage("/images/leaf" + (n) + ".png");
+        this.img_h = window.innerHeight * 0.1;
+        this.img_w = this.img.width * (this.img_h / this.img.height);
         // this is to save div elements inside the object so that it can be accessed easily
         this.div_cont = document.createElement('div');; // create div for leaf
         this.leaf = document.createElement('img'); // create image
         this.nameTag = document.createElement('p'); // create text tag
     }
 
-    display = () => {
-
+    update = () => {
+        if (this.loc.y >= this.g) {
+            this.speed.y = 0;
+            this.loc.y = this.loc.g;
+        } else {
+            this.speed.y = 3;
+        }
+        this.loc.y += this.speed.y;
+        this.loc.x += this.speed.x;
     }
 
-    update = () => {
+    display = () => {
+        this.update(); // socket.on('leafupdate', n) will call this and pass n(aka array index) as argument to edit n
 
+        image(this.image, this.loc.x, this.loc.y, this.img_w, this.img_h);
     }
 }
+
+/*
+image(self.img, self.x - self.img_w//2 - game.x_shift, self.y - self.img_h//2, self.img_w, self.img_h, (self.frame + 1) * self.img_w, 0, self.frame * self.img_w, self.img_h)   
+
+
+(1) check cropping image on processing for background image resizing
+(2) finish p5 set up functions and create obj array+obj
+(3) test motion of leaves here
+(4) add sockets at commented areas
+(5) add mouse functions and click functions
+(6) add rooms and save data to session storage and join selected room
+
+*/
